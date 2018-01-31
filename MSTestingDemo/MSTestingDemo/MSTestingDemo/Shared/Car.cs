f1c92b412144f0b9869e82c15f851ee296ca5858
Kalyani Chintagumpala <Kalyani_Chintagumpala@epam.com>
2018-01-31 12:21:07 +0530

    asdf

---
+++ MSTestingDemo/MSTestingDemo/MSTestingDemo/Shared/Car.cs
@@ -0,0 +1,54 @@
+﻿using MSTestingDemo.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestingDemo
+{
+	public abstract class Car
+	{
+		private decimal _startingPrice = 100000;
+		public abstract string GetSpecifications();
+
+		public virtual decimal GetPrice()
+		{
+			return _startingPrice;
+		}
+
+		public string FeatureMarket()
+		{
+			return "Electric";
+		}
+	}
+
+	public class Maruthi : Car
+	{
+		private CarService _carService;
+		public Maruthi(CarService carService)
+		{
+			_carService = carService;
+		}
+		public override string GetSpecifications()
+		{
+			return "Maruthi Swift specifications";
+		}
+		public override decimal GetPrice()
+		{
+			return 1000000;
+		}
+		public bool SaveNewModel()
+		{
+			return _carService.Save();
+		}
+		public string GetModelName()
+		{
+			return _carService.GetCarModelByID(1);
+		}
+		public bool GetByID()
+		{
+			return _carService.GetByID(1);
+		}
+	}
+}
